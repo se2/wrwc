0e3bdacde4cc0b9803211dbecbea0e57ff0167a5
Tu Luong <tu@delindesign.com>
2018-07-18 09:16:05 -0400

    Add links to Thumbnail Columns

--- themes/wrwc-theme/template-parts/blocks/thumbnail-columns.php
+++ themes/wrwc-theme/template-parts/blocks/thumbnail-columns.php
@@ -23,9 +23,31 @@ $thumbnails = get_sub_field( 'thumbnails' );
 		<div class="grid-x medium-up-5">
 			<?php foreach ( $thumbnails as $key => $thumbnail ) : ?>
 			<div class="cell text-center">
-				<div class="thumbnail bg-center bg-cover" style="background-image:url('<?php echo esc_attr( $thumbnail['thumbnail']['sizes']['thumbnail'] ); ?>');"></div>
+				<div class="thumbnail bg-center bg-cover" style="background-image:url('<?php echo esc_attr( $thumbnail['thumbnail']['sizes']['thumbnail'] ); ?>');">
+					<?php
+					if ( $thumbnail['thumbnail_link'] ) {
+					?>
+					<a href="<?php echo esc_url( $thumbnail['thumbnail_link'] ); ?>"></a>
+					<?php
+					}
+					?>
+				</div>
+				<?php
+				if ( $thumbnail['thumbnail_link'] ) {
+				?>
+				<a href="<?php echo esc_url( $thumbnail['thumbnail_link'] ); ?>">
+				<?php
+				}
+				?>
 				<p class="subtitle"><?php echo esc_html( $thumbnail['thumbnail_subtitle'] ); ?></p>
 				<h6 class="title mb0 uppercase"><?php echo esc_html( $thumbnail['thumbnail_title'] ); ?></h6>
+				<?php
+				if ( $thumbnail['thumbnail_link'] ) {
+				?>
+				</a>
+				<?php
+				}
+				?>
 			</div>
 			<?php endforeach; ?>
 		</div>
