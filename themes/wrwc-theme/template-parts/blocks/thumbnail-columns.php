703617a8c134801867d7307361f1ec491e673946
Tu Luong <tu@delindesign.com>
2018-07-19 10:48:21 -0400

    Responsive Thumbnail Columns section

--- themes/wrwc-theme/template-parts/blocks/thumbnail-columns.php
+++ themes/wrwc-theme/template-parts/blocks/thumbnail-columns.php
@@ -14,13 +14,13 @@ $thumbnails = get_sub_field( 'thumbnails' );
 ?>
 <div class="page-block page-block--thumbnails" style="<?php echo esc_attr( $bg ); ?>">
 	<div class="container">
-		<div class="grid-x grid-centered">
+		<div class="grid-x grid-centered padding-mobile">
 			<div class="cell text-center">
 				<h1 class="uppercase lh1 knockout-title"><?php the_sub_field( 'title' ); ?></h1>
 			</div>
 		</div>
 		<?php if ( $thumbnails ) : ?>
-		<div class="grid-x medium-up-5">
+		<div class="grid-x small-up-2 medium-up-5 grid-centered padding-mobile page-block--thumbnails__grid">
 			<?php foreach ( $thumbnails as $key => $thumbnail ) : ?>
 			<div class="cell text-center">
 				<div class="thumbnail bg-center bg-cover" style="background-image:url('<?php echo esc_attr( $thumbnail['thumbnail']['sizes']['thumbnail'] ); ?>');">
