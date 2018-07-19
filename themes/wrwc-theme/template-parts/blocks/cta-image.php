626e024e5adb6056925fb9325edbb808d6f2f00a
Tu Luong <tu@delindesign.com>
2018-07-19 09:59:18 -0400

    Responsive CTA Image section

--- themes/wrwc-theme/template-parts/blocks/cta-image.php
+++ themes/wrwc-theme/template-parts/blocks/cta-image.php
@@ -12,16 +12,16 @@
 $bg = 'background-color:' . get_sub_field( 'background' ) . ';';
 ?>
 <div class="page-block page-block--cta-image pos-rel" style="<?php echo esc_attr( $bg ); ?>">
+	<?php if ( get_sub_field( 'featured_image' ) ) : ?>
+	<div class="page-block--cta-image__img bg-center bg-cover" style="background-image:url('<?php the_sub_field( 'featured_image' ); ?>');"></div>
+	<?php endif; ?>
 	<div class="container page-block--cta-image__heading">
-		<div class="grid-x grid-margin-x flex-center-items">
-			<div class="cell medium-5">
+		<div class="grid-x flex-center-items page-block--cta-image__content padding-mobile">
+			<div class="cell large-5 text-center--medium">
 				<h1 class="uppercase lh1" style="color:<?php the_sub_field( 'title_color' ); ?>;"><?php the_sub_field( 'title' ); ?></h1>
 				<p class="fz-24" style="color:<?php the_sub_field( 'subtitle_color' ); ?>;"><?php the_sub_field( 'subtitle' ); ?></p>
-				<?php the_cta( get_sub_field( 'cta' ), 'button mb0' ); ?>
+				<?php the_cta( get_sub_field( 'cta' ), 'button large mb0' ); ?>
 			</div>
 		</div>
 	</div>
-	<?php if ( get_sub_field( 'featured_image' ) ) : ?>
-	<div class="pos-abs page-block--cta-image__img bg-right bg-cover" style="background-image:url('<?php the_sub_field( 'featured_image' ); ?>');"></div>
-	<?php endif; ?>
 </div>
