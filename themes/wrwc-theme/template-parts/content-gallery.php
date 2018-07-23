6230eda31e168c39f7df8194086265ab318861a0
Tu Luong <tu@delindesign.com>
2018-07-23 14:05:52 -0400

    Responsive archive & single gallery

--- themes/wrwc-theme/template-parts/content-gallery.php
+++ themes/wrwc-theme/template-parts/content-gallery.php
@@ -8,7 +8,7 @@
 
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="gallery-item border-bottom">
+	<div class="gallery-item <?php echo ( ! is_single() ) ? 'border-bottom' : ''; ?>">
 		<?php if ( ! is_single() ) : ?>
 		<h4 class="uppercase light-gray gallery-title"><?php the_title(); ?></h4>
 		<?php endif; ?>
@@ -38,7 +38,7 @@
 		<?php endif; ?>
 		<?php if ( ! is_single() ) : ?>
 		<div class="grid-x grid-margin-x grid-centered">
-			<div class="cell medium-2">
+			<div class="cell small-6 medium-3 large-2">
 				<a href="<?php the_permalink(); ?>" class="button w100p gallery-see-more">See More</a>
 			</div>
 		</div>
