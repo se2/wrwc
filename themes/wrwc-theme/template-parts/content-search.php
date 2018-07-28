75c8d7c92ced4d0874eb6029f9a516c60de31c1e
Tu Luong <tu@delindesign.com>
2018-07-27 23:24:12 -0400

    Highlight events date in search

--- themes/wrwc-theme/template-parts/content-search.php
+++ themes/wrwc-theme/template-parts/content-search.php
@@ -9,16 +9,16 @@
  */
 
 // Remove /?highlight param from Relevanssi.
-$permalink  = strtok( get_permalink( $post ), '?' );
-$read_more  = 'Keep reading';
-$target     = '';
+$permalink = strtok( get_permalink( $post ), '?' );
+$read_more = 'Keep reading';
+$target    = '';
 
 if ( 'post' === get_post_type( $post ) ) {
 	$permalink = get_the_permalink( $post );
 }
 if ( get_field( 'publication' ) ) {
-	$permalink  = get_field( 'publication' )['url'];
-	$target     = '_blank';
+	$permalink = get_field( 'publication' )['url'];
+	$target    = '_blank';
 }
 if ( 'staff' === get_post_type( $post ) ) {
 	$permalink = '/staff';
@@ -42,18 +42,22 @@ if ( 'page' === get_post_type( $post ) ) {
 					$the_date = get_field( 'event_date' ) ? get_field( 'event_date' ) : get_the_date( 'F j, Y' );
 				?>
 				<!-- https://wordpress.stackexchange.com/questions/52489/the-date-not-working -->
+				<?php if ( function_exists( 'relevanssi_highlight_terms' ) ) : ?>
+				<h6 class="post-date bold uppercase secondary-color"><?php echo relevanssi_highlight_terms( $the_date, get_search_query() ); ?></h6>
+				<?php else : ?>
 				<h6 class="post-date bold uppercase secondary-color"><?php echo esc_html( $the_date ); ?></h6>
-        <?php endif; ?>
-        <?php if ( function_exists( 'relevanssi_the_title' ) ) : ?>
-        <a href="<?php echo esc_url( $permalink ); ?>" target="<?php echo esc_attr( $target ); ?>"><h2 class="uppercase post-title"><?php relevanssi_the_title(); ?></h2></a>
-        <?php else : ?>
-        <a href="<?php echo esc_url( $permalink ); ?>" target="<?php echo esc_attr( $target ); ?>"><h2 class="uppercase post-title"><?php the_title(); ?></h2></a>
-        <?php endif; ?>
+				<?php endif; ?>
+				<?php endif; ?>
+				<?php if ( function_exists( 'relevanssi_the_title' ) ) : ?>
+				<a href="<?php echo esc_url( $permalink ); ?>" target="<?php echo esc_attr( $target ); ?>"><h2 class="uppercase post-title"><?php relevanssi_the_title(); ?></h2></a>
+				<?php else : ?>
+				<a href="<?php echo esc_url( $permalink ); ?>" target="<?php echo esc_attr( $target ); ?>"><h2 class="uppercase post-title"><?php the_title(); ?></h2></a>
+				<?php endif; ?>
 				<?php the_cta( get_field( 'publication' ), 'post-publication bold secondary-color' ); ?>
 				<p>
-          <?php
+					<?php
 					if ( get_the_excerpt() ) {
-            $excerpt = get_the_excerpt();
+						$excerpt = get_the_excerpt();
 						echo $excerpt . '... ';
 					?>
 					<a href="<?php echo esc_url( $permalink ); ?>" class="read-more" target="<?php echo esc_attr( $target ); ?>"><?php echo esc_html( $read_more . ' »' ); ?></a>
